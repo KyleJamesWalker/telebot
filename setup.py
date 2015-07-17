f2e8f560ae37e3f03bd7b9fbb8f4536d2bf1cf86
Kyle Walker <Kyle.Walker@zefr.com>
2015-07-17 06:35:47 -0700

    New Version.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_requirements = [
 
 setup(
     name='telebot',
-    version='0.0.1',
+    version='0.0.2',
     description='A Telegram bot library, with simple route decorators.',
     long_description=readme,
     author='Kyle James Walker',
